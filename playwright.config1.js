6554054947c4558f10e9f3e6a6ead678a7b94a79
priyanka gautam <priyanka.gautamqa@outlook.com>
2025-05-18 23:49:22 +0530

    other config - mobile screen configs, ssl certificate error,permissions

--- playwright.config1.js
+++ playwright.config1.js
@@ -21,24 +21,51 @@ import { defineConfig, devices, expect } from '@playwright/test';
           timeout:5000
       },
 
+      /**
+       *our current website is not  mobile friendly hence test might fail in iphone11 config - browser will open with that screen configurations
+       *Two ways to config bbrowser screen -  
+       *1. viewport:{width:1000,height:700}
+        *2. ...devices['iPhone 14']
+       */
+
+        /**
+         * SSL CERTIFICATE ERROR
+         * Where on opening the websote we get message - your connection is not private ...
+         * click on advacne and accept the terms
+         * PROPERTY to handle such case : ignoreHttpsErrors:true
+         */
+
+        /**
+         * Sometimes on opening the website we get popup - google wants to know your location --
+         * Allow or not allowed 
+         * PROPERTY to handle such case :
+         * permissions:['geolocation']
+         * 
+         */
+
       projects:[
           {
-            name:'chrome execution',
+            name:'chrome',
             use:{
                   browserName:'chromium',//
                   headless:false,
                   screenshot:'on',
-                  trace:'retain-on-failure' //will generate trace.zip only for failed test cases
+                  trace:'retain-on-failure', //will generate trace.zip only for failed test cases
+                  // viewport:{width:1000,height:700}
+                 //...devices['iPhone 14']
+                 //permissions:['geolocation']
+                
                }
           },
 
             {
-            name:'safari execution',
+            name:'safari',
             use:{
                   browserName:'webkit',//
                   headless:true,
                   screenshot:'off',
-                  trace:'retain-on-failure' //will generate trace.zip only for failed test cases
+                  trace:'retain-on-failure', //will generate trace.zip only for failed test cases
+                  //ignoreHTTPSErrors:true
                }
           }
 
